dc1fbddd5d4ff946b4ee475548179dfbc1fd7e9c
Your Name <you@example.com>
2025-07-01 12:49:47 +0530

    done

---
+++ classwork/3.datatypes.py
@@ -0,0 +1,51 @@
+#Numeric Types
+# int
+numbers=444
+type(numbers)
+
+#   float
+PERCENTAGE=70.55
+STRICKRATE=100.00
+
+#COMPLEX
+formula=3+9j
+type(formula)
+
+
+#.Sequence Type
+#strings
+name="sheshu reddy"
+course="data analytics"
+institute="codegnan"
+
+#LIST=mutable
+# to store lists of items such as products in a cart, wishlist, or categories.
+cart_items=["shirt","pants"]
+shopping_categories=["home","electronics","toys"]
+
+#TUPLE = IMMUTABLE
+cart_items=("shirt","pants")
+shopping_categories=("home","electronics","toys")
+
+# SETS
+available_sizes = {"S", "M", "L", "XL"}
+popular_brands = {"Nike", "Adidas", "Puma", "Nike"}
+
+#frozenset – Immutable Set
+frozen_tags = frozenset(["Sale", "Limited Edition",
+"Trending"])
+
+#dict – Dictionary
+product_details = {
+"name": "Wireless Mouse",
+"brand": "Logitech",
+"price": 899.99,
+"rating": 4.5
+}
+#bool – Boolean
+status_payment=True
+status_refund=False
+
+#NoneType – Represents a null or missing value
+tracking_number = None
+delivery_partner = None
