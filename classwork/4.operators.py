989f9c95c6ab9c6ad3c68bfba586d70a011b22ac
Your Name <you@example.com>
2025-07-02 12:38:14 +0530

    operators

---
+++ classwork/4.operators.py
@@ -0,0 +1,48 @@
+#operators
+#arithmetic operators
+
+a=20
+b=10
+print(a+b)#ADDITION(+)
+print(a-b)#SUBTRACTION(-)
+print(a*b)#MULTIPLICATION(*)
+print(a/b)#DIVISION(/)
+print(a//b)#FLOOR DIVISION(//)
+print(a%b)#MODULUS(%)
+print(a**b)#EXPONENTIATION(**)
+
+#comparision Operators
+x=100
+y=200
+print(x==y)#Equal to
+print(x!=y)#not Equal to 
+print(x>y)#greater then
+print(x<y)#lesser then 
+print(x>=y)#Greater then or equal to 
+print(x<=y)#lesser then or equal to
+
+#3.Assignment Operators
+x = 10 #Assign
+x += 5 # Add & Assign
+x *= 2 # Multiply & Assign
+x -= 10 # Subtract & Assign
+x/= 2 # divide and assign 
+x //= 3 #Floor Divide & Assign
+x %= 2 #Modulus & Assign
+x**=3 # Exponentiate & assign
+
+#4. Logical Operators (Using Logic Gates)
+x=10
+(x>9 and x<20) # Returns True if both conditions are true
+(x>9 or x<6) #Returns True if at least one condition is true
+not(x>5)#Reverses the condition (True becomes False)
+
+#5. Membership Operators
+l=1,3,4,6,7,8,54,3,6,8,4,6,55
+3 in l #Returns True if the value exists in the sequence
+l=1,3,4,6,7,8,54,3,6,8,4,6,55
+100 not in l #Returns True if the value does NOT exist in the sequence
+
+
+
+#6. Identity Operators
