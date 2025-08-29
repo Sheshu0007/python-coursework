4c73e5194376e343c2752bcc905133094dea8a80
Your Name <sheshureddy1611@gmail.com>
2025-08-29 16:01:24 +0530

    update

---
+++ classwork/python_lib/num_py.py
@@ -0,0 +1,35 @@
+import numpy as np
+arr1=np.array([1,2,3,4,5,6])
+print(arr1)
+
+zeros=np.zeros((4,4))
+print(zeros)
+
+ones=np.ones((4,4))
+print(ones)
+# Identity matrix
+identity_matrix=np.eye(5)
+print(identity_matrix)
+
+# Array of a specific value
+fully_array=np.full((9,),7)
+print(fully_array)
+
+fuul_array=np.full((2,3),6)
+print(fuul_array)
+
+# Array with range of numbers
+arr=np.arange(1,8,1)
+print(arr)
+
+# Linearly spaced values
+lin_space=np.linspace(0,100,500)
+print(lin_space)
+
+#2. Random Number Generation
+
+# Random integer values
+
+randominteger=np.random.randint(1,100)
+print(randominteger)
+
